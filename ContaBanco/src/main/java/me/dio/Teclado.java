c1ed3ab9709b860f1c7cf314459d1e0438ce3240
Jhonathan <jhonathanfeijomoraes@gmail.com>
2023-08-29 16:55:12 -0400

    Projeto conta banco

---
+++ ContaBanco/src/main/java/me/dio/Teclado.java
@@ -0,0 +1,2 @@
+package me.dio.conta;public class Teclado {
+}
---
+++ ContaBanco/src/main/java/me/dio/conta/Conta.java
@@ -0,0 +1,43 @@
+package me.dio;
+
+import java.math.BigDecimal;
+
+public class Conta {
+
+    private String nome;
+    private String agencia;
+    private int numero;
+    private BigDecimal saldo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+}
